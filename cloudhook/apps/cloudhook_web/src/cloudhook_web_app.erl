2f04dd7d832db871ec00a61271f6a2ebd9b991df
Sebastian Green-Husted <ricecake@tfm.nu>
2016-05-26 17:40:51 -0400

    flesh out simple server that just echos

--- cloudhook/apps/cloudhook_web/src/cloudhook_web_app.erl
+++ cloudhook/apps/cloudhook_web/src/cloudhook_web_app.erl
@@ -1,5 +1,10 @@
 %%%-------------------------------------------------------------------
-%% @doc cloudhook_web public API
+%% @doc cloudhook_web OTP Application callback module.
+%%
+%% Provides the behaviour callbacks required for this
+%% app to be started and managed by the OTP application
+%% management framework.
+%%
 %% @end
 %%%-------------------------------------------------------------------
 
@@ -15,12 +20,37 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    cloudhook_web_sup:start_link().
+	case cloudhook_web_sup:start_link() of
+		{ok, Super} ->
+			{ok, _} = cowboy_start(),
+			{ok, Super}
+	end.
 
 %%--------------------------------------------------------------------
 stop(_State) ->
-    ok.
+	ok.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+cowboy_start() ->
+	{ok, Options} = determine_options(),
+	Dispatch      = cowboy_router:compile([
+		{'_', [
+			{"/api/event",    cloudhook_web_event,       #{}},
+			{"/static/[...]", cowboy_static,             {priv_dir, cloudhook_web, "static/"}}
+		]}
+	]),
+	cowboy:start_http(http, 25, Options, [{env, [{dispatch, Dispatch}]}]).
+
+determine_options() ->
+	WithIp = case application:get_env(cloudhook_web, ip) of
+		undefined -> [];
+		{ok, IpString} when is_list(IpString) ->
+			{ok, Ip} = inet_parse:address(IpString),
+			[{ip, Ip}];
+		{ok, IpTuple} when is_tuple(IpTuple) -> [{ip, IpTuple}]
+	end,
+	Port = application:get_env(cloudhook_web, port, 8888),
+	{ok, [{port, Port} |WithIp]}.
---
+++ cloudhook/apps/cloudhook_web/src/cloudhook_web_event.erl
@@ -0,0 +1,13 @@
+-module(cloudhook_web_event).
+
+-export([
+	init/2
+]).
+
+init(Req, Args) ->
+	{Addr, _Port} = cowboy_req:peer(Req),
+	IP = list_to_binary(inet_parse:ntoa(Addr)),
+	{ok, Body, Req2} = cowboy_req:body(Req),
+	Event = jsx:decode(Body, [return_maps]),
+	io:format("~p~n", [{IP, Event}]),
+	{ok, cowboy_req:reply(401, Req2), Args}.
