1b0b8ddddf2691e1023976fa72e023261aa8cfe5
Sebastian Green-Husted <ricecake@tfm.nu>
2016-05-26 17:53:56 -0400

    return a 204 response

--- cloudhook/apps/cloudhook_web/src/cloudhook_web_event.erl
+++ cloudhook/apps/cloudhook_web/src/cloudhook_web_event.erl
@@ -10,4 +10,4 @@ init(Req, Args) ->
 	{ok, Body, Req2} = cowboy_req:body(Req),
 	Event = jsx:decode(Body, [return_maps]),
 	io:format("~p~n", [{IP, Event}]),
-	{ok, cowboy_req:reply(401, Req2), Args}.
+	{ok, cowboy_req:reply(204, Req2), Args}.
